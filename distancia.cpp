8f11f1861e44647ca89bc5c80bcbe814318af88f
CruzJulian <cruz.julian.mauricio@gmail.com>
2017-01-30 20:07:07 -0500

    Iniciando capítulo de simulación.

---
+++ distancia.cpp
@@ -0,0 +1,24 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+double distan_np(DataFrame df) {
+  
+  // access the columns
+  NumericVector value = df["value"];
+  NumericVector weight = df["weight"];
+  NumericVector binary = df["binary"];
+  
+  int i, n = value.length();
+  double actecdf = weight[0], otherecdf = 0, actbin = binary[0], dis = 0;
+  
+  for(i = 1; i < n; i++){
+    dis += (value[i] - value[i-1])*(actecdf - otherecdf)*(value[i] - value[i-1])*(actecdf - otherecdf);
+    if(binary[i] == actbin){actecdf += weight[i];}
+    if(binary[i] == 1 - actbin){otherecdf += weight[i];}
+  }
+  
+  // return a distance
+  return n*dis;
+}
+
