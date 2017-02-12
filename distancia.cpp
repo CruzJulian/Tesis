ccb547f482078a2c2f7792e571b35707f6e611e1
CruzJulian <cruz.julian.mauricio@gmail.com>
2017-02-12 15:43:26 -0500

    Poner la distancia Earth Movers, que es la que usa EP Means

--- distancia.cpp
+++ distancia.cpp
@@ -19,6 +19,28 @@ double distan_np(DataFrame df) {
   }
   
   // return a distance
+  return sqrt(dis);
+}
+
+
+// [[Rcpp::export]]
+double EMD(DataFrame df) {
+  
+  // access the columns
+  NumericVector value = df["value"];
+  NumericVector weight = df["weight"];
+  NumericVector binary = df["binary"];
+  
+  int i, n = value.length();
+  double actecdf = weight[0], otherecdf = 0, actbin = binary[0], dis = 0;
+  
+  for(i = 1; i < n; i++){
+    dis += (value[i] - value[i-1])*abs(actecdf - otherecdf);
+    if(binary[i] == actbin){actecdf += weight[i];}
+    if(binary[i] == 1 - actbin){otherecdf += weight[i];}
+  }
+  
+  // return a distance
   return dis;
 }
 
