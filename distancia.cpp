8ef1d6eb6cb1f8a4feba7b2e0502bb1d55a1d0a5
CruzJulian <cruz.julian.mauricio@gmail.com>
2017-03-08 22:10:05 -0500

    Varias distancias.

--- distancia.cpp
+++ distancia.cpp
@@ -44,3 +44,30 @@ double EMD(DataFrame df) {
   return dis;
 }
 
+// [[Rcpp::export]]
+double distan_def(DataFrame df) {
+  
+  // access the columns
+  NumericVector value = df["value"];
+  NumericVector weight = df["weight"];
+  NumericVector binary = df["binary"];
+  
+  int i, n_act = 0, n_oth = 0, n = value.length();
+  double actecdf = weight[0], otherecdf = 0, actbin = binary[0], dis = 0;
+  
+  for(i = 1; i < n; i++){
+    dis += (value[i] - value[i-1])*(actecdf - otherecdf)*(actecdf - otherecdf);
+    if(binary[i] == actbin){
+      actecdf += weight[i];
+      n_act++;
+      }
+    if(binary[i] == 1 - actbin){
+      otherecdf += weight[i];
+      n_oth++;
+      }
+  }
+  
+  // return a distance
+  return sqrt(fmin(n_act, n_oth)*dis);
+}
+
